ea8a95f49c6e0909d1e488c95f5c7a940ea4bab2
Kate Richardson <krichardson723@gmail.com>
2016-12-23 17:31:59 -0700

    Added user profiles

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,6 +1,5 @@
 class Reaction < ApplicationRecord
   belongs_to :user
-
   validates :user, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
-  resources :reactions
-  resources :articles
-  resources :tweets
-  ActiveAdmin.routes(self)
-  devise_for :users
-  as :user do
-    get "signin" => 'devise/sessions#new'
-    delete "signout" => 'devise/sessions#destroy'
-    get 'signup' => 'devise/registrations#new'
+    get 'users/:username' => 'users#show', as: user
+
+    resources :reactions
+    resources :articles
+    resources :tweets
+    ActiveAdmin.routes(self)
+    devise_for :users
+    as :user do
+      get "signin", to: 'devise/sessions#new'
+      delete "signout", to: 'devise/sessions#destroy'
+      get 'signup', to: 'devise/registrations#new'
+    end
+    root 'pages#home'
+    get 'about', to: 'pages#about'
+    get 'contact', to: 'pages#contact'
+    get 'tweets', to: 'pages#tweets'
+    get 'articles', to: 'pages#articles'
+    get 'reactions' => 'pages#reactions'
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   end
-  root 'pages#home'
-  get 'about' => 'pages#about'
-  get 'contact' => 'pages#contact'
-  get 'tweets' => 'pages#tweets'
-  get 'articles' => 'pages#articles'
-  get 'reactions' => 'pages#reactions'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
